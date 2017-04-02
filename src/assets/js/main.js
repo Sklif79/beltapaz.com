c177a3e326e5e233edb59a633c267dec27b6bca2
Sklif79 <sklif_@mail.ru>
2017-04-02 12:45:39 +0300

    31.03 работа

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,59 @@
 $(document).ready(function(){
-	
-})
+    mapFooter();
+
+    //sliders
+    $('.promo-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        dotsClass: 'slider-dots',
+        nextArrow: '<div class="slider-next"></div>',
+        prevArrow: '<div class="slider-prev"></div>',
+        arrows: true
+    });
+
+    $('.slider-partners').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: false,
+        nextArrow: '<div class="slider-next"></div>',
+        prevArrow: '<div class="slider-prev"></div>',
+        arrows: true
+    });
+});
+
+
+function mapFooter() {
+    var myMap;
+    ymaps.ready(function () {
+        myMap = new ymaps.Map('mapFooter', {
+            center: [53.71382238397995,23.82329127007925],
+            zoom: 16,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: '',
+                balloonContent: ''
+            }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: '/assets/images/map-marker.png',
+                // Размеры метки.
+                iconImageSize: [25, 37],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                //iconImageOffset: [-3, -42]
+            });
+
+        // myMap.behaviors.disable("scrollZoom");
+
+        myMap.geoObjects.add(myPlacemark);
+    });
+
+}
