63e3dc882f82bbfa8d7ed4f049db564d9b9c6ee0
sklif <sklif_@mail.ru>
2017-04-10 03:24:51 +0300

    10.04 home

--- src/assets/js/libs.js
+++ src/assets/js/libs.js
@@ -1,5 +1,5 @@
 //= libs/jquery-3.2.0.min.js
 //= libs/slick.min.js
-//= libs/uikit/uikit.js
-//= libs/uikit/components/lightbox.js
+
+
 //= libs/jquery-ui.min.js
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
     mapFooter();
+
+    searchActive();
 
     doubleRange();
 
+    if($('.services-images__el').length) {
+        $(window).on('resize', fix_size);
+
+        fix_size('.el_1-3');
+        fix_size('.el_2-3');
+        fix_size('.el_3-3');
+        fix_size('.el_half');
+    }
+
+    $("#card-image").tabs();
+    $("#card-tabs").tabs();
+
+
+
     //sliders
     $('.promo-slider').slick({
-        infinite: true,
+        // infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
         dotsClass: 'slider-dots',
         nextArrow: '<div class="slider-next"></div>',
         prevArrow: '<div class="slider-prev"></div>',
-        arrows: true
+        arrows: true,
+        adaptiveHeight: true,
+        centerMode:true
     });
 
     $('.slider-partners').slick({
@@ -35,11 +53,6 @@ $(document).ready(function(){
         arrows: true
     });
 
-
-
-
-
-
 });
 
 
@@ -47,7 +60,7 @@ function mapFooter() {
     var myMap;
     ymaps.ready(function () {
         myMap = new ymaps.Map('mapFooter', {
-            center: [53.71382238397995,23.82329127007925],
+            center: [53.71382238397995, 23.82329127007925],
             zoom: 16,
             controls: []
         }, {
@@ -78,17 +91,71 @@ function mapFooter() {
 
 //двойной ползунок
 function doubleRange() {
-    $( "#slider-range" ).slider({
+    $("#slider-range").slider({
         range: true,
         min: 80,
         max: 500,
-        values: [ 120, 400 ],
-        slide: function( event, ui ) {
-            $( "#range-min" ).val( ui.values[ 0 ] );
-            $( "#range-max" ).val(  ui.values[ 1 ] );
+        values: [120, 400],
+        slide: function (event, ui) {
+            $("#range-min").val(ui.values[0]);
+            $("#range-max").val(ui.values[1]);
         }
     });
 
-    $( "#range-min" ).val( $( "#slider-range" ).slider( "values", 0 ));
-    $( "#range-max" ).val( $( "#slider-range" ).slider( "values", 1 ));
+    $("#range-min").val($("#slider-range").slider("values", 0));
+    $("#range-max").val($("#slider-range").slider("values", 1));
+}
+
+
+//ресайз изображений под размер контейнера
+function fix_size(classParent) {
+    var images = $(classParent + ' img');
+    images.each(setsize);
+
+    function setsize() {
+        var img = $(this),
+            img_dom = img.get(0),
+            container = img.parents(classParent);
+        if (img_dom.complete) {
+            resize();
+        } else img.one('load', resize);
+
+        function resize() {
+            if ((container.width() / container.height()) > (img_dom.width / img_dom.height)) {
+                img.width('100%');
+                img.height('auto');
+            } else {
+                img.height('100%');
+                img.width('auto');
+            }
+            var marginx = (img.width() - container.width()) / -2,
+                marginy = (img.height() - container.height()) / -2;
+            img.css({'margin-left': marginx, 'margin-top': marginy});
+        }
+    }
+}
+
+
+function searchActive() {
+    // var el = $('input.header-bottom__search-el');
+    var el = $('input.header-bottom__search-el');
+    var close = $('div.header-bottom__search-close');
+    var nav = $('nav.header-bottom__nav');
+
+
+    el.on('click', function () {
+        nav.hide().css('position','absolute');
+        el.parent().addClass('focus');
+        $('.header-bottom__search-el').attr('placeholder','Введите название товара...');
+        $('.header-bottom__search-btn').val('Поиск')
+        close.show();
+    });
+
+    close.on('click', function () {
+        el.parent().removeClass('focus');
+        nav.show(600);
+        $('.header-bottom__search-el').attr('placeholder','Поиск...');
+        $('.header-bottom__search-btn').val('');
+        close.hide();
+    });
 }
