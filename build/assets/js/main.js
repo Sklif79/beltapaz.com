f3e4a217275bc21ed12849e2d95cc0c66585e5cf
sklif <sklif_@mail.ru>
2017-04-17 07:58:53 +0300

    17.04 home

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,13 +1,38 @@
 $(document).ready(function () {
+
     mapFooter();
 
     searchActive();
 
     doubleRange();
 
-    // cropExpo();
+    datapickerLang();
 
-    $(".expo__item-news").dotdotdot({
+    $('a.expo-more-group, a.awards-slider-group, a.awards-slider-group').fancybox({
+        closeBtn: true,
+        padding:0,
+        helpers : {
+            overlay : {
+                css : {
+                    'background' : 'rgba(0,0,0,0.5)'
+                }
+            }
+        }
+    });
+
+    $('a.modalbox').fancybox({
+        closeBtn: true,
+        padding:0,
+        helpers : {
+            overlay : {
+                css : {
+                    'background' : 'rgba(0,0,0,0.5)'
+                }
+            }
+        }
+    });
+
+    $(".expo__item-news, .news-item__text").dotdotdot({
         ellipsis: "",
         tolerance: 1
     });
@@ -16,6 +41,10 @@ $(document).ready(function () {
 
     $("#card-image").tabs();
     $("#card-tabs").tabs();
+    $("#send-order").tabs();
+
+
+    $("#datepicker").datepicker();
 
 
     //************************** sliders *********************************
@@ -32,7 +61,7 @@ $(document).ready(function () {
     });
 
     $('.slider-partners').slick({
-        infinite: true,
+        // infinite: true,
         slidesToShow: 5,
         slidesToScroll: 1,
         dots: false,
@@ -42,7 +71,7 @@ $(document).ready(function () {
     });
 
     $('.slider-news').slick({
-        infinite: true,
+        // infinite: true,
         slidesToShow: 6,
         slidesToScroll: 1,
         dots: false,
@@ -52,12 +81,32 @@ $(document).ready(function () {
     });
 
     $('.expo-more-slider').slick({
-        infinite: false,
+        // infinite: false,
         slidesToShow: 4,
         slidesToScroll: 1,
         dots: false,
         nextArrow: '<div class="expo-more-slider-next"></div>',
         prevArrow: '<div class="expo-more-slider-prev"></div>',
+        arrows: true
+    });
+
+    $('.news-page__slider').slick({
+        // infinite: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: false,
+        nextArrow: '<div class="expo-more-slider-next"></div>',
+        prevArrow: '<div class="expo-more-slider-prev"></div>',
+        arrows: true
+    });
+
+    $('.awards-slider, .about-slider').slick({
+        // infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        nextArrow: '<div class="about-slider-next"></div>',
+        prevArrow: '<div class="about-slider-prev"></div>',
         arrows: true
     });
 
@@ -84,7 +133,7 @@ function mapFooter() {
                 // Своё изображение иконки метки.
                 iconImageHref: '/assets/images/map-marker.png',
                 // Размеры метки.
-                iconImageSize: [25, 37],
+                iconImageSize: [25, 37]
                 // Смещение левого верхнего угла иконки относительно
                 // её "ножки" (точки привязки).
                 //iconImageOffset: [-3, -42]
@@ -98,6 +147,7 @@ function mapFooter() {
 
 
 //двойной ползунок
+//http://jqueryui.com/slider/#range
 function doubleRange() {
     $("#slider-range").slider({
         range: true,
@@ -119,48 +169,33 @@ function searchActive() {
     // var el = $('input.header-bottom__search-el');
     var el = $('input.header-bottom__search-el');
     var close = $('div.header-bottom__search-close');
-    var nav = $('nav.header-bottom__nav');
+    var nav = $('#header-bottom__nav');
 
 
     el.on('click', function () {
         nav.hide().css('position', 'absolute');
         el.parent().addClass('focus');
         $('.header-bottom__search-el').attr('placeholder', 'Введите название товара...');
-        $('.header-bottom__search-btn').val('Поиск')
+        $('.header-bottom__search-btn').val('Поиск');
+
         close.show();
     });
 
     close.on('click', function () {
         el.parent().removeClass('focus');
-        nav.show(600);
+        nav.show(250);
         $('.header-bottom__search-el').attr('placeholder', 'Поиск...');
         $('.header-bottom__search-btn').val('');
         close.hide();
     });
 }
 
-// function cropExpo () {
-//     if ( $('.expo__item-news').length ) {
-//
-//         $('.expo__item-news').each( function () {
-//             var size = 170,
-//                 newsContent= $(this),
-//                 newsText = newsContent.text();
-//
-//             if(newsText.length > size){
-//                 newsContent.text(newsText.slice(0, size) + ' ...');
-//             }
-//
-//         });
-//     }
-// }
-
 function faqSpoilar() {
     var panelItem = document.querySelectorAll('.question'),
         active = document.getElementsByClassName('panel-active');
 
-    Array.from(panelItem).forEach(function(item, i, panelItem) {
-        item.addEventListener('click', function(e) {
+    Array.from(panelItem).forEach(function (item, i, panelItem) {
+        item.addEventListener('click', function (e) {
             if (active.length > 0 && active[0] !== this) // если есть активный элемент, и это не тот по которому кликнули
                 active[0].classList.remove('panel-active'); // убрать класс panel-active
 
@@ -168,4 +203,89 @@ function faqSpoilar() {
             this.classList.toggle('panel-active');
         });
     });
+}
+
+function dataPickerRu() {
+    $.datepicker.regional['ru'] = {
+        closeText: 'Закрыть',
+        prevText: '&#x3c;Пред',
+        nextText: 'След&#x3e;',
+        currentText: 'Сегодня',
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+            'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
+            'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+        dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false
+    };
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+}
+
+function dataPickerGer() {
+    $.datepicker.regional['de'] = {
+        closeText: "Schließen",
+        prevText: "&#x3C;Zurück",
+        nextText: "Vor&#x3E;",
+        currentText: "Heute",
+        monthNames: ["Januar", "Februar", "März", "April", "Mai", "Juni",
+            "Juli", "August", "September", "Oktober", "November", "Dezember"],
+        monthNamesShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun",
+            "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+        dayNames: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+        dayNamesShort: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+        dayNamesMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+        weekHeader: "KW",
+        dateFormat: "dd.mm.yy",
+        firstDay: 1,
+        isRTL: false
+    };
+    $.datepicker.setDefaults($.datepicker.regional['de']);
+}
+
+function dataPickerEsp() {
+    $.datepicker.regional['es'] = {
+        closeText: "Cerrar",
+        prevText: "&#x3C;Ant",
+        nextText: "Sig&#x3E;",
+        currentText: "Hoy",
+        monthNames: [ "enero","febrero","marzo","abril","mayo","junio",
+            "julio","agosto","septiembre","octubre","noviembre","diciembre" ],
+        monthNamesShort: [ "ene","feb","mar","abr","may","jun",
+            "jul","ago","sep","oct","nov","dic" ],
+        dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
+        dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
+        dayNamesMin: [ "D","L","M","X","J","V","S" ],
+        weekHeader: "Sm",
+        dateFormat: "dd/mm/yy",
+        firstDay: 1,
+        isRTL: false
+    };
+    $.datepicker.setDefaults($.datepicker.regional['es']);
+}
+
+
+function datapickerLang() {
+    var lang = $('.dropbtn-lang').attr('data-lang');
+    console.log(lang);
+
+    switch (lang) {
+        case 'ru':
+            dataPickerRu();
+            break;
+
+        case 'ger':
+            dataPickerGer();
+            break;
+
+        case 'sp':
+            dataPickerEsp();
+            break;
+
+        default:
+            $.datepicker.setDefaults($.datepicker.regional['']);
+    }
 }
